62f0b84e6114ff2c0e83c719341c25f0fff4cd38
easin <easinalifouzder@gmail.com>
2022-08-02 19:09:07 +0600

    for loop testing

---
+++ for-test.js
@@ -0,0 +1,14 @@
+var i = 0;
+for (i = 0; i < 5; i++) { console.log("for " + "loop " + i) };
+// why print 5
+console.log("Lastly print" + " " + i);
+
+// 11 times run this loop & print 0-10
+// for (var i = 0; i <= 10; i++) { console.log(i); };
+
+// var name = "false";
+// console.log(typeof "name");
+
+// 0 times run this loop
+// var i = 5;
+// for (; i < 5; i++) { console.log(i); };
